b555d49b0b630d8cc05af2feae02c5c6a10eeb53
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2023-07-25 15:40:10 -0300

    refactor: makes store methods private

--- src/app/entity/store.js
+++ src/app/entity/store.js
@@ -1,79 +1,83 @@
 export class Store {
+  #local;
+  #cacheManager;
+  #online;
+
   constructor({ collection, endpoint, params, ttlInSeconds, fetch, local }) {
     this.collection = collection;
     this.params = params;
     this.endpoint = endpoint;
     this.ttlInSeconds = ttlInSeconds || 86400; //24 hours
 
-    this.local = new local(this.collection);
-    this.cacheManager = new local("cacheManager");
-    this.online = new fetch(this.endpoint);
+    this.#local = new local(this.collection);
+    this.#cacheManager = new local("cacheManager");
+    this.#online = new fetch(this.endpoint);
   }
 
-  getLocalData() {
-    const isOnline = window.navigator.onLine;
-    const ttl = this.getTTL();
-
-    if (ttl > this.ttlInSeconds && isOnline) {
-      this.removeCache();
-    }
-
-    return this.local.get().asObject();
-  }
-
-  async getDataOnline() {
-    return (await this.online.get(this.params)).asObject();
-  }
-
-  removeCache() {
-    return this.local.delete();
-  }
-
-  async set() {
-    this.removeCache();
-    const newData = await this.getDataOnline();
-    this.local.set(newData);
-    this.setCacheDate();
-
-    return newData;
-  }
-
-  setForced(newData) {
-    this.removeCache();
-    this.local.set(newData);
-    this.setCacheDate();
-
-    return newData;
-  }
-
-  async getData(onlyFromLocal) {
-    const localData = this.getLocalData();
+  async getData(onlyFromLocal = false) {
+    const localData = this.#getLocalData();
     if (onlyFromLocal) {
       return localData;
     }
 
-    return localData || this.set();
+    return localData || this.#set();
   }
 
-  setCacheDate() {
-    const cacheData = this.cacheManager.get().asObject();
-    this.cacheManager.set({
+  setForced(newData) {
+    this.#removeCache();
+    this.#local.set(newData);
+    this.#setCacheDate();
+
+    return newData;
+  }
+
+  #getLocalData() {
+    const isOnline = window.navigator.onLine;
+    const ttl = this.#getTTL();
+
+    if (ttl > this.ttlInSeconds && isOnline) {
+      this.#removeCache();
+    }
+
+    return this.#local.get().asObject();
+  }
+
+  async #getDataOnline() {
+    return (await this.#online.get(this.params)).asObject();
+  }
+
+  #removeCache() {
+    return this.#local.delete();
+  }
+
+  async #set() {
+    this.#removeCache();
+    const newData = await this.#getDataOnline();
+    this.#local.set(newData);
+    this.#setCacheDate();
+
+    return newData;
+  }
+
+  #setCacheDate() {
+    const cacheData = this.#cacheManager.get().asObject();
+    this.#cacheManager.set({
       ...cacheData,
-      [this.collection]: this.getThisDateInSeconds(),
+      [this.collection]: this.#getThisDateInSeconds(),
     });
   }
 
-  getTTL() {
+  #getTTL() {
     const lastUpdate =
-      this.cacheManager.get().asObject()?.[this.collection] ||
-      this.getThisDateInSeconds();
-    const timeNow = this.getThisDateInSeconds();
+      this.#cacheManager.get().asObject()?.[this.collection] ||
+      this.#getThisDateInSeconds();
+    const timeNow = this.#getThisDateInSeconds();
     const differenceInSeconds = Math.abs(lastUpdate - timeNow);
 
     return differenceInSeconds;
   }
 
-  getThisDateInSeconds() {
+  #getThisDateInSeconds() {
     return Math.round(new Date().getTime() / 1000);
   }
 }
