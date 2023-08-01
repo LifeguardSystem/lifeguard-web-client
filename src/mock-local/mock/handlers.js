3c1048730322b732c90b6ec42ebf915d02c70a03
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2023-08-01 13:43:09 -0300

    chore: adds mock for another group

--- src/mock-local/mock/handlers.js
+++ src/mock-local/mock/handlers.js
@@ -1,7 +1,9 @@
+import { bees } from "./groupIndividual/bees.js";
 import { jbp } from "./groupIndividual/jbp.js";
 import { groups } from "./groups/groups.js";
 
 export const mockHandlers = {
   "/groups": groups,
   "/groups/?name=JBP": jbp,
+  "/groups/?name=bees": bees,
 };
