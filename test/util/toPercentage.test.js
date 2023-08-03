53b4eb27de8c2409336b9af6b06498912e71c060
Diego Rubin <contact@diegorubin.dev>
2023-08-03 10:39:49 -0300

    fix: typo

--- test/util/toPercentage.test.js
+++ test/util/toPercentage.test.js
@@ -1,5 +1,5 @@
 import { toPercentage } from "../../_site/app/util/to-percentage.js";
-import { region } from "../../_site/app/global/global.js";
+import { region } from "../../_site/app/GLOBAL/global.js";
 
 describe("To percentage testing", () => {
   it("Should return an unformatted result of a percentage", () => {
